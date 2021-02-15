660b5281d11574990a6c6262da9e803d65f4bf7e
nazar <nazikkln@gmail.com>
2021-02-15 17:46:10 +0100

    Alto push(Aggiunta ajax nel JS)

--- BeautySpa/src/main/java/web/controller/ServiziController.java
+++ BeautySpa/src/main/java/web/controller/ServiziController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,5 +22,36 @@ public class ServiziController {
 		DBManager.getInstance().utenteDAO().save(utente);
 		return "";
 		*/
+	}
+	
+	@PostMapping("verificaLogin")
+	public String verirficaLogin(String email, String password)
+	{
+		String verifica;
+		if(email!=null)
+			verifica="SUCCSESS";
+		else
+			verifica="NO";
+		
+		return verifica;
+	}
+	
+	@PostMapping("registrazioneGoogle")
+	public String registrazioneGoogle(String id, String nome, String email)
+	{
+		String ritorna="";
+		String []lista=nome.split(" ");
+		Utente utente= new Utente(email, id, lista[0],lista[1],true,"");
+		System.out.println("Nome: "+utente.getNome()+" Cognome: "+utente.getCognome());
+		//DBManager.getInstance().UtenteDAO().save(utente);
+		Utente ut= DBManager.getInstance().UtenteDAO().trovaUtente(email);
+		if(ut.getEmail()==null)
+			ritorna="Utente e gia registrato";
+		else
+		{
+			DBManager.getInstance().UtenteDAO().save(utente);
+			ritorna="Utente e stato registrato";
+		}
+		return ritorna;
 	}
 }
---
+++ BeautySpa/src/main/webapp/JavaScript/Utenti.js
@@ -0,0 +1,32 @@
+
+function verificaLogin()
+{
+	var ema= document.querySelector("#email").value;
+	var pass= document.querySelector("#password").value;
+	//var studente= new Studente(matr, nome, cogn, date, scuola);
+	
+	$.ajax(
+	{
+		//eticketta ,variabbile
+		url: "verificaLogin",
+		method: "POST",
+		//data: studente,
+		//contentType:"json",
+		data: {email: ema, password: pass},
+		success: function(responce)
+		{
+			if(responce==="SUCCESS")
+			{
+				alert("ha funzionato");
+			}
+			else
+				alert("non funziona!");
+		
+		},
+		fail:function(jqXHR, textStatus)
+		{
+			alert("Request failed:"+ textStatus);
+		}
+	}
+	);
+}
